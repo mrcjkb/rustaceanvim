eb0baedc7f3a7de5049d20b2245dd9cb08595638
Marc Jakobi <marc.jakobi@tiko.energy>
2023-10-23 00:21:25 +0200

    tests(lsp): add assertions for notify and notify_once not being called

--- spec/lsp_spec.lua
+++ spec/lsp_spec.lua
@@ -10,11 +10,15 @@ describe('LSP client API', function()
   if vim.fn.executable(ra_bin) ~= 0 then
     it('Can spin up rust-analyzer.', function()
       local lsp_start = stub(vim.lsp, 'start')
+      local notify_once = stub(vim, 'notify_once')
+      local notify = stub(vim, 'notify')
       vim.cmd.e('test.rs')
       vim.cmd.RustAnalyzer('start')
       -- TODO: Use something less flaky, e.g. a timeout
       uv.sleep(5000)
       assert.stub(lsp_start).was_called()
+      assert.stub(notify_once).was_not_called()
+      assert.stub(notify).was_not_called()
     end)
   end
 end)
