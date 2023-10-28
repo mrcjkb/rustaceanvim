093909dd663ece831320feb08a76dc44dc07a6dd
Marc Jakobi <marc.jakobi@tiko.energy>
2023-10-28 11:55:53 +0200

    fix(explain-error): compatibility layer for vim.system

--- lua/rustaceanvim/compat.lua
+++ lua/rustaceanvim/compat.lua
@@ -11,4 +11,20 @@ M.get_clients = vim.lsp.get_clients or vim.lsp.get_active_clients
 
 M.uv = vim.uv or vim.loop
 
+M.system = vim.system
+  -- wrapper around vim.fn.system to give it a similar API as vim.system
+  or function(cmd, _, on_exit)
+    local output = vim.fn.system(cmd)
+    local ok = vim.v.shell_error
+    ---@type vim.SystemCompleted
+    local systemObj = {
+      signal = 0,
+      stdout = ok and (output or '') or nil,
+      stderr = not ok and (output or '') or nil,
+      code = vim.v.shell_error,
+    }
+    on_exit(systemObj)
+    return systemObj
+  end
+
 return M
