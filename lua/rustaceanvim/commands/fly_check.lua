7cee45171986b93c78f50a218b478a996cf02df6
Marc Jakobi <marc.jakobi@tiko.energy>
2023-12-05 12:14:27 +0100

    fix(flyCheck): correct typo in LSP client request

--- lua/rustaceanvim/commands/fly_check.lua
+++ lua/rustaceanvim/commands/fly_check.lua
@@ -7,7 +7,7 @@ local rl = require('rustaceanvim.rust_analyzer')
 ---@param cmd flyCheckCommand
 function M.fly_check(cmd)
   local params = cmd == 'run' and vim.lsp.util.make_text_document_params() or {}
-  rl.notify('rust-analyzer/' .. cmd .. 'FlyCheck', params)
+  rl.notify('rust-analyzer/' .. cmd .. 'Flycheck', params)
 end
 
 return M.fly_check
