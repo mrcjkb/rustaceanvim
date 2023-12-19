9afd89a036a2c9363ea631a5a7ad96c66c9eebc3
Marc Jakobi <marc.jakobi@tiko.energy>
2023-12-19 22:05:50 +0100

    fix(health): don't eagarly load modules

--- lua/rustaceanvim/health.lua
+++ lua/rustaceanvim/health.lua
@@ -1,9 +1,5 @@
 ---@mod rustaceanvim.health Health checks
 
-local types = require('rustaceanvim.types.internal')
----@type RustaceanConfig
-local config = require('rustaceanvim.config.internal')
-
 local health = {}
 
 local h = vim.health or require('health')
@@ -17,9 +13,6 @@ local warn = h.warn or h.report_warn
 ---@field optional fun():boolean Function that returns whether the dependency is optional
 ---@field url string URL (markdown)
 ---@field info string Additional information
-
-local adapter = types.evaluate(config.dap.adapter)
----@cast adapter DapExecutableConfig | DapServerConfig | boolean
 
 ---@type LuaDependency[]
 local lua_dependencies = {
@@ -40,80 +33,6 @@ local lua_dependencies = {
 ---@field url string URL (markdown)
 ---@field info string Additional information
 ---@field extra_checks function|nil Optional extra checks to perform if the dependency is installed
-
----@type ExternalDependency[]
-local external_dependencies = {
-  {
-    name = 'rust-analyzer',
-    get_binaries = function()
-      local default = { 'rust-analyzer' }
-      if not config then
-        return default
-      end
-      local cmd = types.evaluate(config.server.cmd)
-      if not cmd or #cmd == 0 then
-        return default
-      end
-      return { cmd[1] }
-    end,
-    optional = function()
-      return false
-    end,
-    url = '[rust-analyzer](https://rust-analyzer.github.io/)',
-    info = 'Required by the LSP client.',
-  },
-  {
-    name = 'Cargo',
-    get_binaries = function()
-      return { 'cargo' }
-    end,
-    optional = function()
-      return true
-    end,
-    url = '[Cargo](https://doc.rust-lang.org/cargo/)',
-    info = [[
-      The Rust package manager.
-      Required by rust-analyzer for non-standalone files, and for debugging features.
-      Not required in standalone files.
-    ]],
-  },
-  {
-    name = 'rustc',
-    get_binaries = function()
-      return { 'rustc' }
-    end,
-    optional = function()
-      return true
-    end,
-    url = '[rustc](https://doc.rust-lang.org/rustc/what-is-rustc.html)',
-    info = [[
-      The Rust compiler.
-      Called by `:RustLsp explainError`.
-    ]],
-  },
-}
-if adapter ~= false then
-  table.insert(external_dependencies, {
-    name = adapter.name or 'debug adapter',
-    get_binaries = function()
-      if adapter.type == 'executable' then
-        ---@cast adapter DapExecutableConfig
-        return { 'lldb', adapter.command }
-      else
-        ---@cast adapter DapServerConfig
-        return { 'codelldb', adapter.executable.command }
-      end
-    end,
-    optional = function()
-      return true
-    end,
-    url = '[lldb](https://lldb.llvm.org/)',
-    info = [[
-      A debug adapter (defaults to: LLDB).
-      Required for debugging features.
-    ]],
-  })
-end
 
 ---@param dep LuaDependency
 local function check_lua_dependency(dep)
@@ -178,7 +97,8 @@ local function check_external_dependency(dep)
   end
 end
 
-local function check_config()
+---@param config RustaceanConfig
+local function check_config(config)
   start('Checking config')
   if vim.g.rustaceanvim and not config.was_g_rustaceanvim_sourced then
     error('vim.g.rustaceanvim is set, but it was sourced after rustaceanvim was initialized.')
@@ -205,16 +125,97 @@ local function check_for_conflicts()
 end
 
 function health.check()
+  local types = require('rustaceanvim.types.internal')
+  local config = require('rustaceanvim.config.internal')
+
   start('Checking for Lua dependencies')
   for _, dep in ipairs(lua_dependencies) do
     check_lua_dependency(dep)
   end
 
   start('Checking external dependencies')
+
+  local adapter = types.evaluate(config.dap.adapter)
+  ---@cast adapter DapExecutableConfig | DapServerConfig | boolean
+
+  ---@type ExternalDependency[]
+  local external_dependencies = {
+    {
+      name = 'rust-analyzer',
+      get_binaries = function()
+        local default = { 'rust-analyzer' }
+        if not config then
+          return default
+        end
+        local cmd = types.evaluate(config.server.cmd)
+        if not cmd or #cmd == 0 then
+          return default
+        end
+        return { cmd[1] }
+      end,
+      optional = function()
+        return false
+      end,
+      url = '[rust-analyzer](https://rust-analyzer.github.io/)',
+      info = 'Required by the LSP client.',
+    },
+    {
+      name = 'Cargo',
+      get_binaries = function()
+        return { 'cargo' }
+      end,
+      optional = function()
+        return true
+      end,
+      url = '[Cargo](https://doc.rust-lang.org/cargo/)',
+      info = [[
+      The Rust package manager.
+      Required by rust-analyzer for non-standalone files, and for debugging features.
+      Not required in standalone files.
+    ]],
+    },
+    {
+      name = 'rustc',
+      get_binaries = function()
+        return { 'rustc' }
+      end,
+      optional = function()
+        return true
+      end,
+      url = '[rustc](https://doc.rust-lang.org/rustc/what-is-rustc.html)',
+      info = [[
+      The Rust compiler.
+      Called by `:RustLsp explainError`.
+    ]],
+    },
+  }
+
+  if adapter ~= false then
+    table.insert(external_dependencies, {
+      name = adapter.name or 'debug adapter',
+      get_binaries = function()
+        if adapter.type == 'executable' then
+          ---@cast adapter DapExecutableConfig
+          return { 'lldb', adapter.command }
+        else
+          ---@cast adapter DapServerConfig
+          return { 'codelldb', adapter.executable.command }
+        end
+      end,
+      optional = function()
+        return true
+      end,
+      url = '[lldb](https://lldb.llvm.org/)',
+      info = [[
+      A debug adapter (defaults to: LLDB).
+      Required for debugging features.
+    ]],
+    })
+  end
   for _, dep in ipairs(external_dependencies) do
     check_external_dependency(dep)
   end
-  check_config()
+  check_config(config)
   check_for_conflicts()
 end
 
