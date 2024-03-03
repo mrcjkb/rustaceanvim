1f2e522bb67f335a221527e7772f198e183f7ce9
Marc Jakobi <marc.jakobi@tiko.energy>
2024-03-03 16:58:45 +0100

    fix(lsp): clear Cargo.toml buffer autocommands before creating them

--- ftplugin/toml.lua
+++ ftplugin/toml.lua
@@ -6,13 +6,19 @@ end
 local config = require('rustaceanvim.config.internal')
 local ra = require('rustaceanvim.rust_analyzer')
 if config.tools.reload_workspace_from_cargo_toml then
+  local group = vim.api.nvim_create_augroup('RustaceanCargoReloadWorkspace', { clear = false })
+  local bufnr = vim.api.nvim_get_current_buf()
+  vim.api.nvim_clear_autocmds {
+    buffer = bufnr,
+    group = group,
+  }
   vim.api.nvim_create_autocmd('BufWritePost', {
     buffer = vim.api.nvim_get_current_buf(),
+    group = group,
     callback = function()
       if #ra.get_active_rustaceanvim_clients(nil) > 0 then
         vim.cmd.RustLsp { 'reloadWorkspace', mods = { silent = true } }
       end
     end,
-    group = vim.api.nvim_create_augroup('RustaceanCargoReloadWorkspace', { clear = false }),
   })
 end
