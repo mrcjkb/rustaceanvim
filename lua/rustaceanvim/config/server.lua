116615a6f4db070edd262f1198e3d47bfce02bec
Rachel Powers <508861+Ryex@users.noreply.github.com>
2024-05-19 19:27:31 -0700

    fix(enable_clippy): use correct rust-analyzer config key
    The correct config key for a rust-analyzer check command table is `check`. `checkOnSave` is a boolean
     (there are some hints in the manual that `checkOnSave` as a table is aliased to `check` but even if it is the correct key is `check`)
    
    this prevents incorrect config conflicts downstream

--- lua/rustaceanvim/config/server.lua
+++ lua/rustaceanvim/config/server.lua
@@ -22,13 +22,15 @@ function server.load_rust_analyzer_settings(project_root, opts)
   local default_settings = opts.default_settings or config.server.default_settings
   local use_clippy = config.tools.enable_clippy and vim.fn.executable('cargo-clippy') == 1
   ---@diagnostic disable-next-line: undefined-field
-  if default_settings['rust-analyzer'].checkOnSave == nil and use_clippy then
+  if default_settings['rust-analyzer'].check == nil and use_clippy then
     ---@diagnostic disable-next-line: inject-field
-    default_settings['rust-analyzer'].checkOnSave = {
+    default_settings['rust-analyzer'].check = {
       allFeatures = true,
       command = 'clippy',
       extraArgs = { '--no-deps' },
     }
+    ---@diagnostic disable-next-line: inject-field
+    default_settings['rust-analyzer'].checkOnSave = true
   end
   if not project_root then
     return default_settings
