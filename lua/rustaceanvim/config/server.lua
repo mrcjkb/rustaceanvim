3b5e51cba2c0cb94677033060d6ecd9f857ed166
Rachel Powers <508861+Ryex@users.noreply.github.com>
2024-05-23 09:59:39 -0700

    fix(enable_clippy): use correct rust-analyzer config key (#403)

--- lua/rustaceanvim/config/server.lua
+++ lua/rustaceanvim/config/server.lua
@@ -22,13 +22,21 @@ function server.load_rust_analyzer_settings(project_root, opts)
   local default_settings = opts.default_settings or config.server.default_settings
   local use_clippy = config.tools.enable_clippy and vim.fn.executable('cargo-clippy') == 1
   ---@diagnostic disable-next-line: undefined-field
-  if default_settings['rust-analyzer'].checkOnSave == nil and use_clippy then
+  if
+    default_settings['rust-analyzer'].check == nil
+    and use_clippy
+    and type(default_settings['rust-analyzer'].checkOnSave) ~= 'table'
+  then
     ---@diagnostic disable-next-line: inject-field
-    default_settings['rust-analyzer'].checkOnSave = {
+    default_settings['rust-analyzer'].check = {
       allFeatures = true,
       command = 'clippy',
       extraArgs = { '--no-deps' },
     }
+    if type(default_settings['rust-analyzer'].checkOnSave) ~= 'boolean' then
+      ---@diagnostic disable-next-line: inject-field
+      default_settings['rust-analyzer'].checkOnSave = true
+    end
   end
   if not project_root then
     return default_settings
