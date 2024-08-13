61425a021e21cb5aa689c1fa1171bafcebe19ae0
jannes <jannes.dev@posteo.net>
2024-08-13 10:04:08 -0600

    fix(diagnostic): pass correct flycheck arg for clear/cancel

--- lua/rustaceanvim/commands/fly_check.lua
+++ lua/rustaceanvim/commands/fly_check.lua
@@ -6,7 +6,7 @@ local rl = require('rustaceanvim.rust_analyzer')
 
 ---@param cmd rustaceanvim.flyCheckCommand
 function M.fly_check(cmd)
-  local params = cmd == 'run' and vim.lsp.util.make_text_document_params() or {}
+  local params = cmd == 'run' and vim.lsp.util.make_text_document_params() or nil
   rl.notify('rust-analyzer/' .. cmd .. 'Flycheck', params)
 end
 
