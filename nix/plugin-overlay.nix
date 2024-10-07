d1f56672638508a7bc971cde31a29df4018579a9
Marc Jakobi <marc.jakobi@tiko.energy>
2024-10-07 19:58:52 +0200

    build(nix): use luaAttr

--- nix/plugin-overlay.nix
+++ nix/plugin-overlay.nix
@@ -20,21 +20,25 @@
   lua5_1 = prev.lua5_1.override {
     packageOverrides = rustaceanvim-luaPackage-override;
   };
+  luajit = prev.luajit.override {
+    packageOverrides = rustaceanvim-luaPackage-override;
+  };
 
   lua51Packages = final.lua5_1.pkgs;
+  luajitPackages = final.luajit.pkgs;
 in {
   inherit
     lua5_1
     lua51Packages
+    luajit
+    luajitPackages
     ;
 
   vimPlugins =
     prev.vimPlugins
     // {
       rustaceanvim = final.neovimUtils.buildNeovimPlugin {
-        pname = name;
-        version = "scm-1";
-        src = self;
+        luaAttr = final.luajitPackages.rustaceanvim;
       };
     };
 
