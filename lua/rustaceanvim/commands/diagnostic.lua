01ebc765018039aa2a442ce857e868e7f2850c9c
Marc Jakobi <mrcjkb89@outlook.com>
2024-12-04 12:23:07 +0100

    fix(`relatedDiagnostics`): compatibility with Nvim 0.10.2 (#605)

--- lua/rustaceanvim/commands/diagnostic.lua
+++ lua/rustaceanvim/commands/diagnostic.lua
@@ -459,7 +459,11 @@ function M.related_diagnostics()
     end)
   else
     vim.fn.setqflist({}, ' ', { title = 'related diagnostics', items = quickfix_entries })
-    vim.cmd.botright('copen')
+    if vim.cmd.botright then
+      vim.cmd.botright('copen')
+    else
+      vim.cmd.copen()
+    end
   end
 end
 
