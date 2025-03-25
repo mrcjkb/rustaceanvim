d4622ad6310842ce17a35d9c9bd01b255c6e8cb5
fys <fengys1996@gmail.com>
2025-03-25 23:56:04 +0800

    fix: wrong flycheck params

--- lua/rustaceanvim/commands/fly_check.lua
+++ lua/rustaceanvim/commands/fly_check.lua
@@ -2,11 +2,15 @@ local M = {}
 
 local rl = require('rustaceanvim.rust_analyzer')
 
+local function make_flycheck_params()
+  return { textDocument = vim.lsp.util.make_text_document_params() }
+end
+
 ---@alias rustaceanvim.flyCheckCommand 'run' | 'clear' | 'cancel'
 
 ---@param cmd rustaceanvim.flyCheckCommand
 function M.fly_check(cmd)
-  local params = cmd == 'run' and vim.lsp.util.make_text_document_params() or nil
+  local params = cmd == 'run' and make_flycheck_params() or nil
   rl.notify('rust-analyzer/' .. cmd .. 'Flycheck', params)
 end
 
