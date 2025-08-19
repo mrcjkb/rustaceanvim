eb9beab7d80eb052f78165b28d18f55844b26aef
Marc Jakobi <marc@jakobi.dev>
2025-08-19 12:29:48 +0200

    chore: update flake.lock (#832)
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- troubleshooting/minimal.lua
+++ troubleshooting/minimal.lua
@@ -1,4 +1,5 @@
 vim.env.LAZY_STDPATH = '.repro'
+---@diagnostic disable-next-line: param-type-mismatch
 load(vim.fn.system('curl -s https://raw.githubusercontent.com/folke/lazy.nvim/main/bootstrap.lua'))()
 
 require('lazy.minit').repro {
