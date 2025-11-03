82eb9a516ac0aec9591e11b7d25715443a28a771
Marc Jakobi <marc.jakobi@tiko.energy>
2025-11-03 20:19:08 +0100

    ci: use lux

--- nix/ci-overlay.nix
+++ nix/ci-overlay.nix
@@ -1,8 +1,5 @@
 # Add flake.nix test inputs as arguments here
-{
-  self,
-  plugin-name,
-}: final: prev: let
+final: prev: let
   nvim-nightly = final.neovim-nightly;
 
   inherit (final) lib;
@@ -44,47 +41,7 @@
           '';
         });
 
-  mkNeorocksTest = {
-    name,
-    nvim ? final.neovim-unwrapped,
-  }: let
-    nvim-wrapped = final.pkgs.wrapNeovim nvim {
-      configure = {
-        packages.myVimPackage = {
-          start = [
-            # Add plugin dependencies that aren't on LuaRocks here
-          ];
-        };
-      };
-    };
-  in
-    final.pkgs.neorocksTest {
-      inherit name;
-      pname = plugin-name;
-      src = self;
-      neovim = nvim-wrapped;
-
-      # luaPackages = ps: with ps; [];
-      extraPackages = with final; [
-        rust-analyzer
-        cargo
-      ];
-
-      preCheck = ''
-        export HOME=$(realpath .)
-      '';
-
-      buildPhase = ''
-        mkdir -p $out
-        cp -r tests $out
-      '';
-    };
 in {
-  nvim-stable-tests = mkNeorocksTest {name = "neovim-stable-tests";};
-  nvim-nightly-tests = mkNeorocksTest {
-    name = "neovim-nightly-tests";
-    nvim = nvim-nightly;
-  };
   nvim-minimal-stable = mkNvimMinimal final.neovim-unwrapped;
   nvim-minimal-nightly = mkNvimMinimal nvim-nightly;
 }
