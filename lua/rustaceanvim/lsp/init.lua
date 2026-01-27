f64689bb4707c5d53c6ba3e89afc00c20ef2d9b7
fys <40801205+fengys1996@users.noreply.github.com>
2026-01-27 23:49:09 +0800

    fix(lsp): don't start client without workspace root if `standalone` is disabled (#960)
    Co-authored-by: Marc Jakobi <marc@jakobi.dev>

--- lua/rustaceanvim/lsp/init.lua
+++ lua/rustaceanvim/lsp/init.lua
@@ -174,6 +174,17 @@ M.start = function(bufnr)
   local lsp_start_config = vim.tbl_deep_extend('force', {}, client_config)
   cargo.get_config_root_dir(client_config, bufname, function(root_dir)
     if not root_dir then
+      if not client_config.standalone then
+        vim.notify(
+          [[
+rustaceanvim:
+No project root found.
+Not starting rust-analyzer client because detached/standalone mode is disabled.
+]],
+          vim.log.levels.ERROR
+        )
+        return
+      end
       vim.notify(
         [[
 rustaceanvim:
